bb1640f5c013c9af0df976061a95a7ca9b1cba70
jose <hrjose_2497@hotmail.com>
2019-08-23 10:48:21 -0500

    Laberinto 1.0

---
+++ Assets/Scripts/Esfera.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Esfera : MonoBehaviour
+{
+    Rigidbody RB;
+    // Start is called before the first frame update
+    void Start()
+    {
+        RB = GetComponent<Rigidbody>();
+    }
+
+
+    void FixedUpdate()
+    {
+        RB.AddForce(0, -0.001f, 0);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Finish")
+        {
+            Debug.Log("llegaste a la meta");
+        }
+    }
+}
---
+++ Assets/Scripts/Laberinto.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laberinto : MonoBehaviour
+{
+    public float velocidad;
+
+    void start(){
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float z = Input.GetAxis("Horizontal")*-1*velocidad*Time.deltaTime;
+        float x = Input.GetAxis("Vertical")*velocidad*Time.deltaTime;
+
+        transform.Rotate(new Vector3(x,0,z));
+    }
+}
